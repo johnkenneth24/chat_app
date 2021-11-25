6398e0b104c1447a1b9f25831627dbe84faf4ca0
johnkenneth24 <kengerero@yahoo.com>
2021-11-25 18:38:09 +0800

    broadcasting

--- src/index.js
+++ src/index.js
@@ -15,15 +15,24 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
  
+
 io.on('connection', (socket) => {
 	console.log('New WebSocket connection')
 
+
 	socket.emit('message', 'Welcome!')
+	socket.broadcast.emit('message', 'A new user has joined!')
+
 
 	socket.on('sendMessage', (message) => {
 		io.emit('message', message)
 
 	})
+
+	socket.on('disconnect', () => {
+		io.emit('message', 'A user has left!')
+	})
+
  
 })
 
