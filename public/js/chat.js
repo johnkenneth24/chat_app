9bc0cc2f146631cf0f8c2c819a8035f7c58d5e1a
johnkenneth24 <kengerero@yahoo.com>
2021-11-25 18:22:14 +0800

    sending message with inpur text and button in a console

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,15 @@
 const socket = io()
 
-socket.on('countUpdated', (count) => {
-	console.log('The count has been updated', count)
+socket.on('message', (message) => {
+	console.log(message)
 })
 
-document.querySelector('#increement').addEventListener('click', () => {
-	console.log('Clicked')
-	socket.emit('increement')
+document.querySelector('#message_form').addEventListener('submit', (e) => {
+	e.preventDefault()
+
+	const message = e.target.elements.message.value
+
+	socket.emit('sendMessage', message)
+
 })
+ 
